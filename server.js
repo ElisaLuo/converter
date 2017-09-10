b11e71cde18a71ff47a91e1a7531beaff221839d
Elisa L <luolixuan2003@gmail.com>
2017-09-10 14:39:46 +0000

    slight changes

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/particles.json'));
 })
