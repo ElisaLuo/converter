e0aa816fa4d3154b962a2f9e0a308326c30d0cda
Elisa L <luolixuan2003@gmail.com>
2017-09-10 15:28:36 +0000

    added comment

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const path = require('path');
 
 //app set up
-app.use(express.static(__dirname+'/dist'));
-app.use(express.static(__dirname+'/src'));
+app.use(express.static(__dirname+'/dist'));//uploads angular content
+app.use(express.static(__dirname+'/src'));//uploads src folder content
 app.listen(process.env.PORT || 8080);
 
+//sets up route for app
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
